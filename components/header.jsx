68955530e2b410bc74bd76b7fe39fe2b523e2382
Prithxvhie44 <44prithviraj44@gmail.com>
2025-03-27 21:36:34 +0530

    Layouts made, app route added not found page made db connected and all tables made

--- components/header.jsx
+++ components/header.jsx
@@ -3,9 +3,12 @@ import Image from "next/image"
 import Link from "next/link"
 import { Button } from "./ui/button"
 import { LayoutDashboard, PenBox } from "lucide-react"
+import { checkUser } from "@/lib/checkUser"
 
 
-const Header = () => {
+const Header = async () => {
+    await checkUser();
+    
     return (
         <div className="fixed top-0 w-full bg-white/80 z-50 border-b background-blur-md">
             <nav className="container mx-auto flex items-center justify-between py-4 px-4">
