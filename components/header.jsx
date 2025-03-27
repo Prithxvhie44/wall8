0aac8b5d814e8822eceee102d01082b80189c889
Prithxvhie44 <44prithviraj44@gmail.com>
2025-03-27 20:07:48 +0530

    Landing page made 1.13

--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,57 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { LayoutDashboard, PenBox } from "lucide-react"
 
 
 const Header = () => {
     return (
-        <div className="fixed top-0"> <SignedOut>
-            <SignInButton />
-            <SignUpButton />
-        </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn></div>
+        <div className="fixed top-0 w-full bg-white/80 z-50 border-b background-blur-md">
+            <nav className="container mx-auto flex items-center justify-between py-4 px-4">
+                <Link href="/">
+                    <Image
+                        src={"/logo.png"}
+                        alt="Welth Logo"
+                        width={200}
+                        height={60}
+                        className="h-12 w-auto object-contain"
+                    />
+                </Link>
+
+
+
+                <div className="flex items-center space-x-4">
+                    <SignedIn>
+                        <Link href={"/dashboard"} >
+                            <Button variant="outline" className="text-gray-800 hover:text-blue-600 flex items-center gap-2">
+                                <LayoutDashboard size={18}/>
+                                <span className="hidden md:inline">Dashboard</span></Button>
+                        </Link>
+
+
+                        <Link href={"/transactions/create"} >
+                            <Button  className="flex items-center gap-2">
+                                <PenBox size={18}/>
+                                <span className="hidden md:inline">Add Transaction</span></Button>
+                        </Link>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl="/dashboard">
+                            <Button variant="outline">Login</Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                    <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },}}/>
+                    </SignedIn>
+                </div>
+            </nav>
+        </div>
+
     )
 }
 
